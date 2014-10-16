2241dc59201aedba050d19a7fb90a6e6847229a2
PaulAsjes <p.asjes@gmail.com>
2014-10-16 16:46:18 -0400

    Added data json and messed about with the background

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+function BeerTeller() {
+	"use strict";
+
+	var tree = {};
+
+	this.init = function() {
+		document.body.style.background = "#" + randomHexColour();
+	}
+
+	function randomHexColour() {
+		return Math.random().toString(16).slice(-6);
+	}
+
+	function next() {
+
+	}
+
+	function restart() {
+
+	}
+}
+
+window.onload = function() {
+	var beerTeller = new BeerTeller();
+	beerTeller.init();
+}
