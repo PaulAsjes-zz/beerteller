ffb8fd56ef02399e27ea7f12e269fce11db84dff
PaulAsjes <p.asjes@gmail.com>
2014-11-17 17:26:10 -0500

    Removed cofee script stuff and moved data json file to a more sensible place

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,12 @@ function BeerTeller() {
 
 	this.init = function() {
 		document.body.style.background = "#" + randomHexColour();
+
+		callAjax("data/data.json", function(data) {
+			tree = data;
+		});
+
+		console.log(tree);
 	}
 
 	function randomHexColour() {
@@ -17,6 +23,19 @@ function BeerTeller() {
 
 	function restart() {
 
+	}
+
+	function callAjax(url, callback){
+	    var xmlhttp;
+	    // compatible with IE7+, Firefox, Chrome, Opera, Safari
+	    xmlhttp = new XMLHttpRequest();
+	    xmlhttp.onreadystatechange = function(){
+	        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+	            callback(xmlhttp.responseText);
+	        }
+	    }
+	    xmlhttp.open("GET", url, true);
+	    xmlhttp.send();
 	}
 }
 
